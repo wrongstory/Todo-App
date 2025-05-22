afe825c27bc6e9506cd2c820c543465b92f7a16e
L <lmwoo0327@naver.com>
2025-05-22 19:01:29 +0900

    1. 변경점 : 토글 기능 확인. [warring 1. input에 수정문을 넣어도 수정 버튼으로 토글 시, 내용이 초기화됨]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
   const [todoList, setTodoList] = useState([
-    { id: 0, content: "123" },
-    { id: 1, content: "코딩 공부하기" },
-    { id: 2, content: "잠 자기" },
+    { id: 0, content: '123' },
+    { id: 1, content: '코딩 공부하기' },
+    { id: 2, content: '잠 자기' },
   ]);
 
   return (
@@ -18,7 +18,7 @@ function App() {
 }
 
 function TodoInput({ todoList, setTodoList }) {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
 
   return (
     <>
@@ -31,7 +31,7 @@ function TodoInput({ todoList, setTodoList }) {
           const newTodo = { id: Number(new Date()), content: inputValue };
           const newTodoList = [...todoList, newTodo];
           setTodoList(newTodoList);
-          setInputValue("");
+          setInputValue('');
         }}
       >
         추가하기
@@ -51,16 +51,21 @@ function TodoList({ todoList, setTodoList }) {
 }
 
 function Todo({ todo, setTodoList }) {
-  const [inputValue, setInputValue] = useState("");
+  // 1. 변경점 : 수정 버튼을 클릭한 경우에만 input 이 보이게
+  const [showEdit, setShowEdit] = useState(false);
+  const [inputValue, setInputValue] = useState();
+
   return (
     <li>
       {todo.content}
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
+
+      {/* 1-2. 변경점 : 초기에 input 안보이게 */}
+      {showEdit && <InputToDo />}
+
       <button
         onClick={() => {
+          // 1-3. 변경점 : 토글 기능 (warring! input에 수정문을 넣어도 내용이 초기화됨)
+          setShowEdit((prev) => !prev);
           setTodoList((prev) =>
             prev.map((el) =>
               el.id === todo.id ? { ...el, content: inputValue } : el
@@ -80,6 +85,18 @@ function Todo({ todo, setTodoList }) {
         삭제
       </button>
     </li>
+  );
+}
+
+// 1-1. 변경점 : input 기능을 컴포넌트화
+function InputToDo() {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <input
+      value={inputValue}
+      onChange={(event) => setInputValue(event.target.value)}
+    />
   );
 }
 
