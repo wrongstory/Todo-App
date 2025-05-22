cce39bed1ec49c2698f5f97cc132d6e24d2df578
L <lmwoo0327@naver.com>
2025-05-22 19:21:19 +0900

    수정 버튼 기능 완성

--- src/App.jsx
+++ src/App.jsx
@@ -53,27 +53,38 @@ function TodoList({ todoList, setTodoList }) {
 function Todo({ todo, setTodoList }) {
   // 1. 변경점 : 수정 버튼을 클릭한 경우에만 input 이 보이게
   const [showEdit, setShowEdit] = useState(false);
-  const [inputValue, setInputValue] = useState();
+  // 1-5. 변경점 : useState() -> useState('') _ uncontrolled 에서 controlled로 상태 전환
+  const [inputValue, setInputValue] = useState('');
+
+  // 1-4. 변경점 : 토글 기능
+  const handleEditToggle = () => {
+    setShowEdit((prev) => !prev);
+  };
 
   return (
     <li>
       {todo.content}
 
       {/* 1-2. 변경점 : 초기에 input 안보이게 */}
-      {showEdit && <InputToDo />}
+      {/* 1-5. 변경점 : props 추가에 맞게 여기도 props 전달 추가 */}
+      {showEdit && (
+        <InputToDo inputValue={inputValue} setInputValue={setInputValue} />
+      )}
 
       <button
         onClick={() => {
+          if (showEdit) {
+            setTodoList((prev) =>
+              prev.map((el) =>
+                el.id === todo.id ? { ...el, content: inputValue } : el
+              )
+            );
+          }
           // 1-3. 변경점 : 토글 기능 (warring! input에 수정문을 넣어도 내용이 초기화됨)
-          setShowEdit((prev) => !prev);
-          setTodoList((prev) =>
-            prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
-            )
-          );
+          handleEditToggle();
         }}
       >
-        수정
+        {showEdit ? '수정완료' : '수정'}
       </button>
       <button
         onClick={() => {
@@ -88,10 +99,8 @@ function Todo({ todo, setTodoList }) {
   );
 }
 
-// 1-1. 변경점 : input 기능을 컴포넌트화
-function InputToDo() {
-  const [inputValue, setInputValue] = useState('');
-
+// 1-5. 변경점 : input 기능을 컴포넌트화 -> 1-3 warring 해결을 위해 props 추가
+function InputToDo({ inputValue, setInputValue }) {
   return (
     <input
       value={inputValue}
